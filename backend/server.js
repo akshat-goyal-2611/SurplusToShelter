d27d8bffc7e140fbc7441db4fbfe6be291bfbc75
Akshat <akshatgoyal902@gmail.com>
2026-09-25 02:26:38 +0530

    Configure backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -247,8 +247,8 @@ app.get("/api/dashboard", async (req, res) => {
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
